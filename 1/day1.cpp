3768b8afe7d3485bea8e24244a8734041f45f271
Pieter <piethboterhoek@gmail.com>
2025-12-01 20:07:08 +0100

    day part 1

---
+++ 1/day1.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <chrono>
+
+using namespace std;
+
+int Check(string dir, int end, int password) {
+    int max = 100;
+    int min = 0;
+    if (dir == "L") {
+        password = password -= end;
+        while (password < min) {
+            int temp = 0;
+            temp = abs(password);
+            password = max - temp;
+        }
+    } else {
+        password = password += end;
+        while (password >= max) {
+            int temp = 0;
+            temp = password - max;
+            password = min + temp;
+        }
+    }
+    cout << password << endl;
+    return password;
+}
+
+int main() {
+    auto start = chrono::high_resolution_clock::now();
+    int password = 50;
+    int count = 0;
+
+    string input;
+    string dir;
+    int pos;
+
+    ifstream MyReadFile("input.txt");
+
+    while (getline (MyReadFile, input)) {
+        cout << input << endl;
+
+        pos = input.find("L");
+        dir = "L";
+        if (pos < 0) {
+            dir = "R";
+        }
+
+        int end = stoi(input.substr(1));
+        //cout << end << endl;
+        
+        password = Check(dir, end, password);
+
+        if (password == 0) {
+            count++;
+        }
+    }
+
+    MyReadFile.close(); 
+
+    //while (true)
+    //{
+    //    cin >> input;
+    //
+    //    if (input == "end") {
+    //        break;
+    //    }
+    //
+    //    pos = input.find("L");
+    //    dir = "L";
+    //    if (pos < 0) {
+    //        pos = input.find("R");
+    //       dir = "R";
+    //    }
+    //
+    //    string end = input.substr(pos + 1);
+    //    
+    //    password = Check(dir, end, password);
+    //
+    //    cout << "het is: " << password << endl;
+    //}
+    cout << "het is: " << count << endl;
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<std::chrono::microseconds>(end - start);
+    cout << duration.count() << endl;
+    return 0;
+}
